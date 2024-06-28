00145bf6535660f1f39b05d6eafa81e786467aa4
Anish K Pillai <anishpillaimca@gmail.com>
2024-06-28 12:49:01 -0400

    Connected MongoDB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const mongoDB = process.env.MONGO_URI;
+
+mongoose
+  .connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Mongo DB is connected'))
+  .catch((err) => console.log('Mondo DB connection failed! ', err));
 
 const app = express();
 const port = 3000;
