d3a9024003a9807b6bfbe596778360c4d4205f65
Anish K Pillai <anishpillaimca@gmail.com>
2024-06-29 12:41:01 -0400

    Created sign-up route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import auth from './routes/auth.route.js';
 
 dotenv.config();
 const mongoDB = process.env.MONGO_URI;
@@ -11,8 +13,12 @@ mongoose
   .catch((err) => console.log('Mondo DB connection failed! ', err));
 
 const app = express();
+app.use(express.json());
+
 const port = 3000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}!`);
 });
+app.use('/api/user', userRouter);
+app.use('/api/auth', auth);
